259113940e16d3e312fe375244cd71d25d21f5f5
amh5366 <amh5366@upenn.edu>
2018-06-26 17:23:02 -0400

    Fixed Time

--- assets/code/logic.js
+++ assets/code/logic.js
@@ -1,4 +1,4 @@
-var getTrainVals = function(event) {
+var getTrainVals = function (event) {
     //prevent refresh when hitting the button
     event.preventDefault();
 
@@ -8,7 +8,7 @@ var getTrainVals = function(event) {
     var firstTrain = document.querySelector("#time-input").value.trim();
     var trainFrequency = document.querySelector("#freq-input").value.trim();
     var train = new trainShedule(trainName, trainDest, firstTrain, trainFrequency);
-    
+
     //store information into our database
     database.ref("/train").push(train);
 }
@@ -16,24 +16,29 @@ var getTrainVals = function(event) {
 
 document.getElementById('sub-train-btn').addEventListener("click", getTrainVals);
 
-database.ref("/train").on('child_added', function(snapshot) {
+database.ref("/train").on('child_added', function (snapshot) {
     var name = snapshot.val().trainName;
     var dest = snapshot.val().trainDestination;
     var frequen = snapshot.val().trainFrequency;
     var trainStart = snapshot.val().firstTrain;
-    console.log(trainStart);
-    var time = moment(trainStart, "HH:mm").add(frequen, "m");
-    
-    console.log('time: ' + time.format('hh:mm'));
+    var time = moment(trainStart, "HH:mm").subtract(1, "years");
+    var nowTime = moment();
+    var timeDiff = moment().diff(moment(time), 'minutes');
+    var timeRemain = timeDiff % frequen;
+    var tillTrain = frequen - timeRemain;
+    var nextTime = nowTime.add(tillTrain, 'minutes').format("hh:mm A");
+
+    var tilNext = tillTrain;
 
     var tableRow = $("<tr>").append(
         $("<td>").attr("scope", "row").text(name),
         $("<td>").attr("scope", "col").text(dest),
         $("<td>").attr("scope", "col").text(frequen),
-       
+        $("<td>").attr("scope", "col").text(nextTime),
+        $("<td>").attr("scope", "col").text(tilNext + " minutes")
     );
-    
+
     $('#train-table > tbody').append(tableRow);
 
-    
+
 })
